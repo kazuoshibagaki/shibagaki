93c3cf79ba3909155736ffe62aef7eff17ed73e8
Seishi Ono <ono@seishiono.net>
2025-09-02 18:02:39 +0900

    initial commit

---
+++ admin.php
@@ -0,0 +1,8 @@
+<?php
+        define('PKWK_READONLY', 0);
+       define('PKWK_SKIN_SHOW_NAVBAR', 1);
+        define('PKWK_SKIN_SHOW_TOOLBAR', 1);
+define('TDIARY_THEME', 'hatena-brown');
+       require('./index_orig.php')
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+   define('PKWK_SKIN_SHOW_NAVBAR', 0);
+   define('PKWK_SKIN_SHOW_TOOLBAR', 0);
+   define('TDIARY_THEME', 'hatena-brown');
+       require('./index_orig.php')
+?>
